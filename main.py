bb9feeb150ae8d9296a82eebee50cf2d996728d7
克二 / Katsujii <katsujii.works@gmail.com>
2026-04-26 04:40:08 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+
+folder_path = "test_folder"  # フォルダ名
+
+files = os.listdir(folder_path)
+
+for i, file in enumerate(files):
+    old_path = os.path.join(folder_path, file)
+    
+    new_name = f"file_{i+1}.jpg"
+    new_path = os.path.join(folder_path, new_name)
+    
+    os.rename(old_path, new_path)
+
+print("完了！")
